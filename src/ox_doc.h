d922d5309701d0b317a0fe87b2cfc22e26fe2dc8
diviner <d@localhost.localdomain>
2016-05-18 12:55:29 +0800

    add ox_doc.c/ox_doc.h

---
+++ src/ox_doc.h
@@ -0,0 +1,26 @@
+#ifndef _OX_DOC_
+#define _OX_DOC_
+
+#include <stdio.h>
+#include <sys/file.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <wand/magick_wand.h>
+
+#include "ox_lua.h"
+#include "ox_cbs.h"
+#include "ox_memc.h"
+#include "ox_utils.h"
+#include "ox_db.h"
+#include "ox_string.h"
+#include "ox_log.h"
+#include "ox_md5.h"
+#include "ox_common.h"
+
+int ox_doc_save(thr_arg_t *thr_arg, const char *buff, const int len, char *md5);
+int ox_doc_new(const char *buff, const size_t len, const char *save_name);
+//int ox_doc_get(ox_req_t *req, evhtp_request_t *request);
+
+#endif
